e9d8936e33f9ddf905600dba7170fbd421be7193
Jesper Hägglund <jeppan875@gmail.com>
2017-04-18 14:10:23 +0200

    initial commit

---
+++ src/main/java/nackademin/se/rest/test/BookOperation.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nackademin.se.rest.test;
+
+import static com.jayway.restassured.RestAssured.delete;
+import static com.jayway.restassured.RestAssured.given;
+import com.jayway.restassured.http.ContentType;
+
+import static com.jayway.restassured.http.ContentType.JSON;
+import com.jayway.restassured.response.Response;
+import java.util.Random;
+import java.util.UUID;
+import nackademin.se.rest.test.models.Book;
+import nackademin.se.rest.test.models.SingleBook;
+
+/**
+ *
+ * @author jesper
+ */
+public class BookOperation {
+
+    final static String BASE_URL = "http://localhost:8080/librarytest/rest/";
+    private String jsonString ="";
+
+    public Response getBookResponse(int id) {
+         Response response = given().accept(ContentType.JSON).get(BASE_URL+"books/"+id);
+ 
+        return response;
+    }
+    public Book getBook(int id) {
+        Book book = given().accept(ContentType.JSON).get(BASE_URL+"books/4").jsonPath().getObject("book",Book.class);
+ 
+        return book;
+    }    
+    public SingleBook createRandomBook(){
+
+        Book book = new Book();
+        book.setDescription(UUID.randomUUID().toString());
+        book.setTitle(UUID.randomUUID().toString());
+        book.setIsbn(UUID.randomUUID().toString());
+        book.setNbOfPage(new Random().nextInt(500));
+        SingleBook singleBook= new SingleBook(book);
+        
+        return singleBook;
+    }
+    public String getLatestJsonString(){
+    return jsonString;
+    }
+    
+    public Response getAllBooks(){
+        String resourceName= "books";
+        Response getResponse = given().accept(ContentType.JSON).get(BASE_URL+resourceName).prettyPeek();
+        return getResponse;
+    }
+    public Response deleteBook(int id){
+         String deleteResourceName = "books/"+id;
+         Response deleteResponse = delete(BASE_URL+deleteResourceName);   
+         return deleteResponse;
+    } 
+}
